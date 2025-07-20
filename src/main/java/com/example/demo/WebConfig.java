1e76140b67a0bd6dde39b731ee856adf00871440
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-07-20 06:15:43 +0530

    new changes

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -7,16 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("https://687c392d8045b448c3b57735--pleasesorryforgive.netlify.app/") // <-- Netlify site
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
